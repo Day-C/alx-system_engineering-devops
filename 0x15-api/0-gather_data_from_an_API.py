749a58c66cd1e17f98b42b65b693989c0f511621
Day-C <kongranalanyuy@gmail.com>
2024-05-21 03:07:32 -0700

    extraract data dtom AIP

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,27 +7,27 @@ import sys
 def show_todo(id):
     '''Show_todo display the todo into about a user with id=id.'''
 
-    #get todo info
+    # get todo info
     url = f'https://jsonplaceholder.typicode.com/todos?userId={id}'
 
-    response = requests.get(url)
-    data = response.json()
-    tal= len(data)
+    response1 = requests.get(url)
+    data = response1.json()
+    tal = len(data)
     complt = 0
 
     for i in range(len(data)):
-        if data[i].get('completed') == True:
+        if data[i].get('completed') is True:
             complt += 1
 
-    #get user info
+    # get user info
     url = f'https://jsonplaceholder.typicode.com/users/{id}'
-    response = requests.get(url)
-    info = dict(response.json())
+    response2 = requests.get(url)
+    info = dict(response2.json())
     user_name = info.get('name')
     text = f'Employee {user_name} is done with tasks'
     print(f'{text}({complt}/{tal}')
     for item in range(len(data)):
-        if data[item].get('completed') == True:
+        if data[item].get('completed') is True:
             print('\t', data[item].get('title'))
 
 
