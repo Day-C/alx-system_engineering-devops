70bf38150ab13118af69ea301af7c59800e90434
Day-C <kongranalanyuy@gmail.com>
2024-05-20 17:19:42 -0700

    collect info from an api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,6 +7,7 @@ import sys
 def show_todo(id):
     '''Show_todo display the todo into about a user with id=id.'''
 
+    #get todo info
     url = f'https://jsonplaceholder.typicode.com/todos?userId={id}'
 
     response = requests.get(url)
@@ -18,7 +19,13 @@ def show_todo(id):
         if data[i].get('completed') == True:
             complt += 1
 
-    print(f'Employee Ervin Howell is done with tasks({complt}/{tal}')
+    #get user info
+    url = f'https://jsonplaceholder.typicode.com/users/{id}'
+    response = requests.get(url)
+    info = dict(response.json())
+    user_name = info.get('name')
+    text = f'Employee {user_name} is done with tasks'
+    print(f'{text}({complt}/{tal}')
     for item in range(len(data)):
         if data[item].get('completed') == True:
             print('\t', data[item].get('title'))
