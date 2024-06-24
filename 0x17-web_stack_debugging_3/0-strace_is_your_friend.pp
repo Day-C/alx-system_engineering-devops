493ad4a9a34f4be32b7c30f2fd10eb6fa1124934
Day-C <kongranalanyuy@gmail.com>
2024-06-24 12:32:19 +0000

    wp run

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -7,3 +7,7 @@ service {7 'apache2':
   ensure => 'running',
   enable => true,
 }
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
+  path    => ['/bin', '/usr/bin', '/usr/sbin']
+}
