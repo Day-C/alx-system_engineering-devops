4d02d9b55e314a9ce6655c0deafc4cf28d873dd7
DayC <kongranalanyuy@gmail.com>
2024-08-18 21:13:11 +0000

    strace_debuger

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,13 +1,22 @@
-#Define package resource to install apache
+/*
+  This puppet manifest sets up an apache server  and 
+  fiexes an error in one of its files that is causing an thhp 500 error
+*/
+
+#ensure Apache is installed
 package { 'apache2':
-  ensure => 'installed',
+  ensure  => 'installed',
 }
-#Define a service resource to  ensure that apache is running
-service {7 'apache2':
-  ensure => 'running',
-  enable => true,
+
+#running the process  of not alrady
+service { 'apache2':
+  ensure  => 'running',
+  enable  => true,
 }
-exec { 'fix-wordpress':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php; sudo service apache2 restart',
-  path    => ['/bin', '/usr/bin', '/usr/sbin']
+
+#correct the error in the file using the sed command
+exec { 'replace_faulty_pattern_in_file':
+  command  => "/bin/sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' wp-settings.php",
+  onlyif   => "/bin/grep -q 'class-wp-locale.phpp' wp-settings.php",
+  cwd      => '/var/www/html',
 }
--- 0x17-web_stack_debugging_3/README.md
+++ 0x17-web_stack_debugging_3/README.md
@@ -1,1 +1,3 @@
 Using 'Strace' to debug an appahe server that returns a 500 error then automate it with puppet.
+First i used strace to debug and find an error then used puppet for fixing the bug.
+": the problem with the file was a type error made in the sttings.php file. i used a puppet manifest to correct the file
