6cc511f361ea10f0d62ad848dbaedbf7c00966f6
DayC <kongranalanyuy@gmail.com>
2024-08-18 21:47:00 +0000

    passed puppet-lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,19 +1,19 @@
 #This puppet manifest sets up an apache server  and 
- #fiexes an error in one of its files that is causing an thhp 500 error
+#fiexes an error in one of its files that is causing an thhp 500 error
 
 package { 'apache2':
-  ensure  => 'installed',
+  ensure => 'installed',
 }
 
 #running the process  of not alrady
 service { 'apache2':
-  ensure  => 'running',
-  enable  => true,
+  ensure => 'running',
+  enable => true,
 }
 
 #correct the error in the file using the sed command
 exec { 'replace_faulty_pattern_in_file':
-  command  => "/bin/sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' wp-settings.php",
-  onlyif   => "/bin/grep -q 'class-wp-locale.phpp' wp-settings.php",
-  cwd      => '/var/www/html',
+  command => "/bin/sed -i 's/class-wp-locale.phpp/class-wp-locale.php/' wp-settings.php",
+  onlyif  => "/bin/grep -q 'class-wp-locale.phpp' wp-settings.php",
+  cwd     => '/var/www/html',
 }
